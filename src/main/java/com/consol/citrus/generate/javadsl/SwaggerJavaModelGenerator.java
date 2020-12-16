94f790f5e7a8d68a0712aa5591424ce7b88d74db
Alexander <karjuk.sasha@gmail.com>
2020-12-16 18:31:11 +0400

    Реализовал черновой вариант генерации объектной модели

--- src/main/java/com/consol/citrus/generate/javadsl/SwaggerJavaModelGenerator.java
+++ src/main/java/com/consol/citrus/generate/javadsl/SwaggerJavaModelGenerator.java
@@ -2,16 +2,18 @@ package com.consol.citrus.generate.javadsl;
 
 import com.consol.citrus.exceptions.CitrusRuntimeException;
 import com.consol.citrus.util.FileUtils;
-import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.*;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.parser.OpenAPIV3Parser;
+import javassist.bytecode.analysis.Type;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.lang.model.element.Modifier;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class SwaggerJavaModelGenerator {
@@ -22,24 +24,58 @@ public class SwaggerJavaModelGenerator {
     public void create() {
         OpenAPI openAPI;
 
+        //TODO: дублировние парсинга, надо вынести
         try {
             openAPI = new OpenAPIV3Parser().readContents(FileUtils.readToString(new PathMatchingResourcePatternResolver()
                     .getResource(swaggerResource)), null, null).getOpenAPI();
         } catch (IOException e) {
             throw new CitrusRuntimeException("Failed to parse Swagger Open API specification: " + swaggerResource, e);
         }
-        openAPI.getComponents().getSchemas();
 
+        //Сомнительная идея
         for (Map.Entry<String, Schema> schema : openAPI.getComponents().getSchemas().entrySet()) {
+            Map<String, Schema> properties = schema.getValue().getProperties();
+            Iterable<FieldSpec> fieldSpecs;
+            List<FieldSpec> list = new ArrayList<>();
+
+            for (Map.Entry<String, Schema> property : properties.entrySet()) {
+                String type = property.getValue().getType();
+                TypeName typeName = TypeName.OBJECT;
+                if (type != null) {
+                    switch (type) {
+                        case "integer":
+                            typeName = property.getValue().getFormat().equals("int32") ? ClassName.get(Integer.class)
+                                    : ClassName.get(Long.class);
+                            break;
+                        case "number":
+                            typeName = ClassName.get(Double.class);
+                            break;
+                        case "string":
+                            typeName = ClassName.get(String.class);
+                            break;
+                        case "boolean":
+                            typeName = ClassName.get(Boolean.class);
+                            break;
+                        case "array":
+                            typeName = ParameterizedTypeName.get(ClassName.get(List.class), ClassName.get(Object.class));
+                            break;
+                    }
+                }
+                list.add(FieldSpec.builder(typeName, property.getKey()).addModifiers(Modifier.PUBLIC).build());
+            }
+
+            fieldSpecs = list;
+
             TypeSpec classBuilder = TypeSpec.classBuilder(schema.getKey())
                     .addModifiers(Modifier.PUBLIC)
+                    .addFields(fieldSpecs)
                     .build();
 
             JavaFile javaFile = JavaFile.builder("com.consol.citrus.model", classBuilder)
                     .build();
 
             try {
-                javaFile.writeTo(new File(directory));
+                javaFile.writeTo(new File(directory)); //new File(directory)
             } catch (IOException e) {
                 e.printStackTrace();
             }

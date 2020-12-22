f38e5ce474007acd9b20da493479ad8cc79bd761
Alexander <karjuk.sasha@gmail.com>
2020-12-22 13:22:17 +0400

    Создал генератор для citrus-context

---
+++ src/main/java/com/consol/citrus/generate/XmlGenerator.java
@@ -0,0 +1,79 @@
+package com.consol.citrus.generate;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+
+public class XmlGenerator {
+    private String directory;
+
+    public void create() {
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+            Document doc = docBuilder.newDocument();
+            Element beansElement = doc.createElement("beans");
+            doc.appendChild(beansElement);
+            beansElement.setAttribute("xmlns", "http://www.springframework.org/schema/beans");
+            beansElement.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            beansElement.setAttribute("xmlns:citrus", "http://www.citrusframework.org/schema/config");
+            beansElement.setAttribute("xmlns:citrus-http", "http://www.citrusframework.org/schema/http/config");
+            beansElement.setAttribute("xsi:schemaLocation", "http://www.springframework.org/schema/beans" +
+                    "   http://www.springframework.org/schema/beans/spring-beans.xsd" +
+                    "   http://www.citrusframework.org/schema/config" +
+                    "   http://www.citrusframework.org/schema/config/citrus-config.xsd" +
+                    "   http://www.citrusframework.org/schema/http/config" +
+                    "   http://www.citrusframework.org/schema/http/config/citrus-http-config.xsd");
+
+            Element repository = doc.createElement("citrus:schema-repository");
+            repository.setAttribute("id", "schemaRepository");
+            repository.setAttribute("type", "json");
+            beansElement.appendChild(repository);
+
+            Element schemas = doc.createElement("citrus:schemas");
+            repository.appendChild(schemas);
+
+            Element client = doc.createElement("citrus-http:client");
+            client.setAttribute("id", "htpClient");
+            client.setAttribute("request-url", "https://petstore3.swagger.io");
+            beansElement.appendChild(client);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(directory));
+
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+            transformer.transform(source, result);
+
+            System.out.println("File saved!");
+
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (TransformerException tfe) {
+            tfe.printStackTrace();
+        }
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(String directory) {
+        this.directory = directory;
+    }
+}
---
+++ src/test/java/com/consol/citrus/generate/XmlGeneratorTest.java
@@ -0,0 +1,19 @@
+package com.consol.citrus.generate;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class XmlGeneratorTest {
+    private String testDir = Paths.get(".").toAbsolutePath().normalize().toString();
+
+    @Test
+    public void testCreateCitrusContext() throws IOException {
+        XmlGenerator xmlGenerator = new XmlGenerator();
+
+        xmlGenerator.setDirectory(testDir + "/src/test/resources/test.xml");
+
+        xmlGenerator.create();
+    }
+}

818f42dec27f16f04092b65a22aba6e891e59058
Alexander <karjuk.sasha@gmail.com>
2020-12-24 13:53:56 +0400

    Добавил метод для генерации pom файла

--- src/main/java/com/consol/citrus/generate/XmlGenerator.java
+++ src/main/java/com/consol/citrus/generate/XmlGenerator.java
@@ -16,55 +16,64 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 
 public class XmlGenerator extends Generator {
-    private final String fileName = "citrus-context.xml";
+    private final String contextFile = "citrus-context.xml";
+    private final String pomFile = "pom.xml";
 
     @Override
     public void create() {
         try {
-            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-
-            Document doc = docBuilder.newDocument();
-            Element beansElement = doc.createElement("beans");
-            doc.appendChild(beansElement);
-            beansElement.setAttribute("xmlns", "http://www.springframework.org/schema/beans");
-            beansElement.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
-            beansElement.setAttribute("xmlns:citrus", "http://www.citrusframework.org/schema/config");
-            beansElement.setAttribute("xmlns:citrus-http", "http://www.citrusframework.org/schema/http/config");
-            beansElement.setAttribute("xsi:schemaLocation", "http://www.springframework.org/schema/beans" +
-                    "   http://www.springframework.org/schema/beans/spring-beans.xsd" +
-                    "   http://www.citrusframework.org/schema/config" +
-                    "   http://www.citrusframework.org/schema/config/citrus-config.xsd" +
-                    "   http://www.citrusframework.org/schema/http/config" +
-                    "   http://www.citrusframework.org/schema/http/config/citrus-http-config.xsd");
-
-            Element repository = doc.createElement("citrus:schema-repository");
-            repository.setAttribute("id", "schemaRepository");
-            repository.setAttribute("type", "json");
-            beansElement.appendChild(repository);
-
-            Element schemas = doc.createElement("citrus:schemas");
-            repository.appendChild(schemas);
-
-            Element client = doc.createElement("citrus-http:client");
-            client.setAttribute("id", "httpClient");
-            client.setAttribute("request-url", "https://petstore3.swagger.io");
-            beansElement.appendChild(client);
-
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File(directory + fileName));
-
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-            transformer.transform(source, result);
-
-            log.info("Successfully created file: " + fileName);
+            createCitrusContext();
         } catch (ParserConfigurationException | TransformerException ex) {
             ex.printStackTrace();
         }
     }
 
+    private void createCitrusContext() throws ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        Document doc = docBuilder.newDocument();
+        Element beansElement = doc.createElement("beans");
+        doc.appendChild(beansElement);
+        beansElement.setAttribute("xmlns", "http://www.springframework.org/schema/beans");
+        beansElement.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        beansElement.setAttribute("xmlns:citrus", "http://www.citrusframework.org/schema/config");
+        beansElement.setAttribute("xmlns:citrus-http", "http://www.citrusframework.org/schema/http/config");
+        beansElement.setAttribute("xsi:schemaLocation", "http://www.springframework.org/schema/beans" +
+                "   http://www.springframework.org/schema/beans/spring-beans.xsd" +
+                "   http://www.citrusframework.org/schema/config" +
+                "   http://www.citrusframework.org/schema/config/citrus-config.xsd" +
+                "   http://www.citrusframework.org/schema/http/config" +
+                "   http://www.citrusframework.org/schema/http/config/citrus-http-config.xsd");
+
+        Element repository = doc.createElement("citrus:schema-repository");
+        repository.setAttribute("id", "schemaRepository");
+        repository.setAttribute("type", "json");
+        beansElement.appendChild(repository);
+
+        Element schemas = doc.createElement("citrus:schemas");
+        repository.appendChild(schemas);
+
+        Element client = doc.createElement("citrus-http:client");
+        client.setAttribute("id", "httpClient");
+        client.setAttribute("request-url", "https://petstore3.swagger.io");
+        beansElement.appendChild(client);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File(directory + contextFile));
+
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+        transformer.transform(source, result);
+
+        log.info("Successfully created file: " + contextFile);
+    }
+
+    //TODO: implement method
+    private void createPom() throws ParserConfigurationException, TransformerException {
+
+    }
 }
